b1e3fa116eec075e184ba46c9385184e0b42e4f9
kaiserkonok <kaiserkonok@gmail.com>
2021-09-08 01:05:11 +0600

    simple react js todo application

--- src/Todos.js
+++ src/Todos.js
@@ -2,15 +2,23 @@ import React from 'react'
 
 function Todos({ props }) {
     const todos = props.todos
-    return (
-        <div className="container mt-5">
-            <ul className="list-group">
-                {todos.map((todo, index) => (
-                    <li className="list-group-item" key={index}>{todo.title}<button className="btn btn-danger btn-sm float-end" onClick={() => props.deleteTodo(index)}>Delete</button></li>
-                ))}
-            </ul>
-        </div>
-    )
+    if (todos.length === 0) {
+        return (
+            <div className="container mt-5">
+                <h2>There is no todo!</h2>
+            </div>
+        )
+    } else {
+        return (
+            <div className="container mt-5">
+                <ul className="list-group">
+                    {todos.map((todo, index) => (
+                        <li className="list-group-item" key={index}>{todo.title}<button className="btn btn-danger btn-sm float-end" onClick={() => props.deleteTodo(index)}>Delete</button></li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default Todos;
